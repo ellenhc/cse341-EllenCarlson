e0e77f216a63a0c983c000f991fa6687ce4faf2d
ellenhc <59638391+ellenhc@users.noreply.github.com>
2021-02-01 17:10:08 -0800

    added return to databaseConnect()

--- web/library/connections.php
+++ web/library/connections.php
@@ -1,19 +1,20 @@
 <?php
 function databaseConnect()
 {
+    $dbUrl = getenv('DATABASE_URL');
+    $dbOpts = parse_url($dbUrl);
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"], '/');
     try {
-        $dbUrl = getenv('DATABASE_URL');
-        $dbOpts = parse_url($dbUrl);
-        $dbHost = $dbOpts["host"];
-        $dbPort = $dbOpts["port"];
-        $dbUser = $dbOpts["user"];
-        $dbPassword = $dbOpts["pass"];
-        $dbName = ltrim($dbOpts["path"], '/');
         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $db;
     } catch (PDOException $e) {
         echo 'Error:' . $e->getMessage();
-        //header('Location: /web/view/500.php');
+        header('Location: /view/500.php');
         die();
     }
 }
---
+++ web/view/500.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <title>Error | BudgetTracker</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/scss/style.css">
+</head>
+
+<body>
+    <header>
+        <?php require $_SERVER['DOCUMENT_ROOT'] . '/snippets/header.php'; ?>
+    </header>
+    <main>
+        <p>This page isn’t working</p>
+    </main>
+    <footer>
+        <?php require $_SERVER['DOCUMENT_ROOT'] . '/snippets/footer.php'; ?>
+    </footer>
+</body>
+
+</html>
