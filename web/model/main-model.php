040dd7ae04116ddabeef88ae98ee79fba3117fcc
ellenhc <59638391+ellenhc@users.noreply.github.com>
2021-02-02 18:36:44 -0800

    fixing array print

--- web/model/main-model.php
+++ web/model/main-model.php
@@ -9,7 +9,7 @@ function getCategories(){
     $stmt->execute();
     $categories = $stmt->fetchAll();
     $stmt->closeCursor();
-    echo $categories;
+    print_r($categories);
     return $categories; //returns data back to the controller
 }
 ?>
