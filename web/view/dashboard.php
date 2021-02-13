4555ae2b370267f0596669651fdfbb09f6a9a0a7
ellenhc <59638391+ellenhc@users.noreply.github.com>
2021-02-12 23:44:22 -0800

    trying out moving search form to dashboard

--- web/view/dashboard.php
+++ web/view/dashboard.php
@@ -25,16 +25,15 @@ if (!$_SESSION['loggedin']) {
 
             var data = google.visualization.arrayToDataTable([
                 ['Categories', 'Amount']
-                <?php 
+                <?php
                 $remove = array("$", ",");
                 //$replace = str_replace($remove, "", $num);
-                foreach($allExpenses as $expense){
-                    if(!$expense[0]){
+                foreach ($allExpenses as $expense) {
+                    if (!$expense[0]) {
                         $num = $expense['sum'];
                         $replace = str_replace($remove, "", $num);
                         echo ", ['$expense[categoryName]', $replace]";
-                    }
-                    else{
+                    } else {
                         $num = $expense['sum'];
                         echo "['$expense[categoryName]', $replace]";
                     }
@@ -65,8 +64,39 @@ if (!$_SESSION['loggedin']) {
             echo "<b>$message</b>";
         }
         ?>
-        <div class=""></div>
-        <div id="piechart"></div>
+        <div class="piechart">
+            <h2>Recent...</h2>
+            <h6>Last 30 days</h6>
+            <div id="piechart"></div>
+        </div>
+        <div class="search">
+            <form action="/expenses/index.php" method="post" class="search-form">
+                <h1>View Transactions</h1>
+                <h3>Limit my search results by:</h3>
+                <label for="expenseName">Name:</label>
+                <input type="text" name="expenseName" id="expenseName">
+
+                <label for="daterange">Date Added:</label>
+                <select name="daterange" id="daterange">
+                    <option value="7">7 days</option>
+                    <option value="30">30 days</option>
+                    <option value="60">60 days</option>
+                    <option value="90">90 days</option>
+                    <option value="365">1 year</option>
+                </select>
+
+                <label for="categoryId">Category:</label>
+                <?php echo $categoryList; ?>
+
+                <label for="userId">Submitter:</label>
+                <?php echo $userList; ?>
+
+                <input type="submit" value="Search">
+
+                <!--The action key-value pair-->
+                <input type="hidden" name="action" value="searchExpenses">
+            </form>
+        </div>
     </main>
     <footer>
         <?php require $_SERVER['DOCUMENT_ROOT'] . '/snippets/footer.php'; ?>
